165565fc20b842a9c18f2ddd8a348e4b40f3e07d
guochao <guochao751082551@163.com>
2024-12-05 10:31:46 +0800

    dev 修改为底部对齐样式

--- JXBanner/Classes/Banner/Transform/JXBannerTransformLinear.swift
+++ JXBanner/Classes/Banner/Transform/JXBannerTransformLinear.swift
@@ -13,24 +13,30 @@ public struct JXBannerTransformLinear: JXBannerTransformable {
     public init() {}
     
     public func transformToAttributes(collectionView: UICollectionView,
-                               params: JXBannerLayoutParams,
-                               attributes: UICollectionViewLayoutAttributes) {
+                                       params: JXBannerLayoutParams,
+                                       attributes: UICollectionViewLayoutAttributes) {
         
         let collectionViewWidth = collectionView.frame.width
-        if collectionViewWidth <= 0 { return }
-        let centetX = collectionView.contentOffset.x + collectionViewWidth * 0.5
-        let delta = abs(attributes.center.x - centetX)
+        let collectionViewHeight = collectionView.frame.height
+        if collectionViewWidth <= 0 || collectionViewHeight <= 0 { return }
+        
+        let centerX = collectionView.contentOffset.x + collectionViewWidth * 0.5
+        let delta = abs(attributes.center.x - centerX)
+        
+        // 计算缩放比例和透明度
         let scale = max(1 - delta / collectionViewWidth * params.rateOfChange, params.minimumScale)
         let alpha = max(1 - delta / collectionViewWidth, params.minimumAlpha)
         
-        var transform: CGAffineTransform = CGAffineTransform(scaleX: scale, y: scale)
+        // 创建缩放变换
+        var transform = CGAffineTransform(scaleX: scale, y: scale)
         var _alpha = alpha
         
-        // Adjust spacing When Scroling
-        let location = JXBannerTransfrom.itemLocation(viewCentetX: centetX,
-                                                      itemCenterX: attributes.center.x)
+        // 计算翻译量
+        let location = JXBannerTransfrom.itemLocation(viewCentetX: centerX, itemCenterX: attributes.center.x)
         let rate = 1.05 + params.rateHorisonMargin
         var translate: CGFloat = 0
+        
+        // 根据位置调整翻译量
         switch location {
         case .left:
             translate = rate * attributes.size.width * (1 - scale) / 2
@@ -39,11 +45,14 @@ public struct JXBannerTransformLinear: JXBannerTransformable {
         case .center:
             _alpha = 1.0
         }
-        transform = transform.translatedBy(x: translate, y: 0)
         
-        // Set transform
-        attributes.transform = transform;
-        attributes.alpha = _alpha;
+        // 计算底部对齐的偏移量
+        let bottomOffset = (attributes.size.height * (1 - scale)) / 2
+        transform = transform.translatedBy(x: translate, y: bottomOffset) // 向上平移以保持底部对齐
+        
+        // 设置变换和透明度
+        attributes.transform = transform
+        attributes.alpha = _alpha
     }
 }
 
